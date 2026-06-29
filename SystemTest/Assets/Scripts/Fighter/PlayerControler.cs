d7c528b20053065e049ed4b4e5e28119ec2069ce
nicolasgarcia-davinci <nicolas.garcia1@davinci.edu.ar>
2026-06-28 22:21:51 -0300

    cambio de pausa y partes del win lose
    ada

--- SystemTest/Assets/Scripts/Fighter/PlayerControler.cs
+++ SystemTest/Assets/Scripts/Fighter/PlayerControler.cs
@@ -17,18 +17,19 @@ public class PlayerControler : MonoBehaviour
     void Update()
     {
         if (IsPaused) return;
+
         if (Input.GetKeyDown(KeyCode.UpArrow)) Character.Attack(Character.Head.AttName, Character.Head.ParticleContainer, ref Character.IsAttackingUp);
         if (Input.GetKeyDown(KeyCode.RightArrow)) Character.Attack(Character.Rarm.AttName, Character.Rarm.ParticleContainer, ref Character.IsAttackingRight);
         if (Input.GetKeyDown(KeyCode.LeftArrow)) Character.Attack(Character.Larm.AttName, Character.Larm.ParticleContainer, ref Character.IsAttackingLeft);
         if (Input.GetKeyDown(KeyCode.DownArrow)) Character.Attack(Character.Leg.AttName, Character.Leg.ParticleContainer, ref Character.IsAttackingDown);
+        
         if (Input.GetKeyDown(KeyCode.W)) Character.Dodge("DoedgeUp", ref Character.IsDodgingUp);
         if (Input.GetKeyDown(KeyCode.D)) Character.Dodge("DoedgeRight", ref Character.IsDodgingRight);
         if (Input.GetKeyDown(KeyCode.A)) Character.Dodge("DoedgeLeft", ref Character.IsDodgingLeft);
         if (Input.GetKeyDown(KeyCode.S)) Character.Dodge("DoedgeDown", ref Character.IsDodgingDown);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            FightControler.Instance.Pause();
-        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) FightControler.Instance.Pause();
+
     }
     public void Pause()
     {
